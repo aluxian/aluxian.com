50bb75b9e218b41cab93f453f8e68f24fc4b20e5
Corey Daley <cdaley@redhat.com>
2013-10-15 11:29:29 -0400

    updating config.js to use production, and setting environment variable for production via pre_start action hook

--- config.js
+++ config.js
@@ -7,7 +7,43 @@ var path = require('path'),
 config = {
     // ### Development **(default)**
     development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
+		// The url to use when providing links to the site, E.g. in RSS and email.
+		        url: 'http://my-ghost-blog.com',
+
+		        // Example mail config
+		        // Visit http://docs.ghost.org/mail for instructions
+		        // ```
+		        //  mail: {
+		        //      transport: 'SMTP',
+		        //      options: {
+		        //          service: 'Mailgun',
+		        //          auth: {
+		        //              user: '', // mailgun username
+		        //              pass: ''  // mailgun password
+		        //          }
+		        //      }
+		        //  },
+		        // ```
+
+		        database: {
+		            client: 'sqlite3',
+		            connection: {
+		                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+		            },
+		            debug: false
+		        },
+		        server: {
+		            // Host to be passed to node's `net.Server#listen()`
+		            host: '127.0.0.1',
+		            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+		            port: '2368'
+		        }
+    },
+
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
         url: 'http://' + process.env.OPENSHIFT_APP_DNS,
         mail: {},
         database: {
@@ -22,27 +58,6 @@ config = {
             host: process.env.OPENSHIFT_NODEJS_IP,
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '8080'
-        }
-    },
-
-    // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
-    production: {
-        url: 'http://my-ghost-blog.com',
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2369'
         }
     },
 
