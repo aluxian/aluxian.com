4977bdb1c36d136847b15b5f4d8deef466c99aa8
Chris Brown <chris@prawnstar.net>
2014-01-25 14:30:48 -0800

    change tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
 var app,
-	changed 	= require('gulp-changed'),
-	clean		= require('gulp-clean'),
-	es 			= require('event-stream'),
-	embedlr		= require('gulp-embedlr'),
-	express		= require('express'),
-	gulp 		= require('gulp'),
-	gulpif 		= require('gulp-if'),
-	gulputil	= require('gulp-util'),
-	isdev		= gulp.env.out !== true,
-	jade 		= require('gulp-jade'),
-	livereload 	= require('gulp-livereload'),
-	lr 			= require('tiny-lr'),
-	path		= require('path'),
-	rename		= require('gulp-rename'),
-	sass 		= require('gulp-sass'),
-	server		= lr(),
-	staticServer,
-	uglify 		= require('gulp-uglify');
+    changed     = require('gulp-changed'),
+    clean       = require('gulp-clean'),
+    es          = require('event-stream'),
+    embedlr     = require('gulp-embedlr'),
+    express     = require('express'),
+    gulp        = require('gulp'),
+    gulpif      = require('gulp-if'),
+    gulputil    = require('gulp-util'),
+    isdev       = gulp.env.out !== true,
+    jade        = require('gulp-jade'),
+    livereload  = require('gulp-livereload'),
+    lr          = require('tiny-lr'),
+    path        = require('path'),
+    rename      = require('gulp-rename'),
+    sass        = require('gulp-sass'),
+    server      = lr(),
+    staticServer,
+    uglify      = require('gulp-uglify');
 
 
 // move static assets
@@ -39,22 +39,22 @@ gulp.task('clean', function() {
 
 // jade to html
 gulp.task('jade', function() {
-	gulp.src('src/jade/*.jade')
-    	.pipe(jade({'pretty':true}))
-		.pipe(livereload(server))
-		.pipe(gulpif(isdev, embedlr()))
-    	.pipe(gulp.dest('dist'));
+    gulp.src('src/jade/*.jade')
+        .pipe(jade({'pretty':true}))
+        .pipe(livereload(server))
+        .pipe(gulpif(isdev, embedlr()))
+        .pipe(gulp.dest('dist'));
 });
 
 
 // compile scss as compressed css
 gulp.task('sass', function() {
-	gulp.src('src/scss/*.scss')
+    gulp.src('src/scss/*.scss')
         .pipe(changed('dist/css'))
-		.pipe(rename('style.css'))
-		.pipe(sass({'outputStyle':'compressed'}))
-		.pipe(livereload(server))
-		.pipe(gulp.dest('dist/css'));
+        .pipe(rename('style.css'))
+        .pipe(sass({'outputStyle':'compressed'}))
+        .pipe(livereload(server))
+        .pipe(gulp.dest('dist/css'));
 });
 
 
@@ -89,18 +89,18 @@ gulp.task('default', function() {
         if (err) {
             return console.log(err)
         };
-		// run all tasks on first run
-		gulp.run('clean', 'sass', 'jade', 'assets', 'uglify');
-		// start watching src files
-		gulp.watch('src/scss/*.scss', function(event){
-		    gulp.run('sass');
-		});
-		gulp.watch('src/js/*.js', function(event){
-		    gulp.run('uglify');
-		});
-		gulp.watch('src/jade/**/*.jade', function(event){
-		    gulp.run('jade');
-		});
+        // run all tasks on first run
+        gulp.run('clean', 'sass', 'jade', 'assets', 'uglify');
+        // start watching src files
+        gulp.watch('src/scss/*.scss', function(event){
+            gulp.run('sass');
+        });
+        gulp.watch('src/js/*.js', function(event){
+            gulp.run('uglify');
+        });
+        gulp.watch('src/jade/**/*.jade', function(event){
+            gulp.run('jade');
+        });
 
-	});
+    });
 });
