4147c912e8337565e7ca9a477d048f01f95c8038
Chris Brown <c.brown@dcclab.com>
2014-07-19 16:25:00 -0700

    new site template

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,31 @@
 /*jslint browser:true */
+/*global $, jQuery*/
 (function ($) {
     "use strict";
 
-    // Declare Vars
+    var target;
 
-    // Functions
+    $(function () {
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+                target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1500);
+                    return false;
+                }
+            }
+        });
+    });
 
-    // Call Functions
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.goTop').fadeIn(200);
+        } else {
+            $('.goTop').fadeOut(200);
+        }
+    });
 
 }(jQuery));
