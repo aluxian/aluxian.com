2f0b13eb5c5e70f1de94cf1d519821d8d0ede1d8
Chris Brown <c.brown@dcclab.com>
2014-07-20 16:59:48 -0700

    removed rename

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,6 @@ var app,
     lrport      = 35729,
     lrserver    = lr(),
     path        = require('path'),
-    rename      = require('gulp-rename'),
     sass        = require('gulp-sass'),
     staticServer,
     uglify      = require('gulp-uglify');
