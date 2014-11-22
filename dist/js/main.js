b0d995a95a9ffe336ff2bae48f2b4674e7dc947e
Alexandru Rosianu <rosian_alex@yahoo.com>
2014-11-22 12:40:26 +0200

    Fixed popup menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -61,72 +61,13 @@
 })();
 
 (function() {
-  var checkMenu, navTrigger, offset, popupNav, popupNavList;
-  offset = 900;
-  popupNav = document.querySelector('.popup-nav.fixed');
-  navTrigger = popupNav.querySelector('.trigger');
-  popupNavList = popupNav.querySelector('nav ul');
-  checkMenu = function() {
-    var animCallback, c, cb, type, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _ref2, _results, _results1;
-    if (window.scrollY > offset && !popupNav.classList.contains('visible')) {
-      popupNav.classList.add('visible');
-      animCallback = function(type) {
-        popupNavList.classList.add('has-transitions');
-        return navTrigger.removeEventListener(type, animCallback);
-      };
-      _ref = ['webkitAnimationEnd', 'oanimationend', 'msAnimationEnd', 'animationend'];
-      _results = [];
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        type = _ref[_i];
-        _results.push(navTrigger.addEventListener(type, animCallback.bind(null, type)));
-      }
-      return _results;
-    } else if (window.scrollY <= offset - 500) {
-      if (popupNavList.classList.contains('is-visible')) {
-        popupNavList.classList.add('is-hidden');
-        cb = function(type) {
-          var c, _j, _len1, _ref1;
-          _ref1 = ['is-visible', 'is-hidden', 'has-transitions'];
-          for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-            c = _ref1[_j];
-            popupNavList.classList.remove(c);
-          }
-          popupNav.classList.remove('visible');
-          navTrigger.classList.remove('menu-is-open');
-          return navTrigger.removeEventListener(type, cb);
-        };
-        _ref1 = ['webkitTransitionEnd', 'otransitionend', 'oTransitionEnd', 'msTransitionEnd', 'transitionend'];
-        _results1 = [];
-        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-          type = _ref1[_j];
-          _results1.push(popupNavList.addEventListener(type, cb.bind(null, type)));
-        }
-        return _results1;
-      } else if (popupNavList.classList.contains('is-visible')) {
-        _ref2 = ['is-visible', 'has-transitions'];
-        for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
-          c = _ref2[_k];
-          popupNavList.classList.remove(c);
-        }
-        popupNav.classList.remove('visible');
-        return navTrigger.classList.remove('menu-is-open');
-      } else {
-        popupNav.classList.remove('visible');
-        return popupNavList.classList.remove('has-transitions');
-      }
-    }
-  };
-  checkMenu();
-  window.onscroll = checkMenu;
-  return navTrigger.addEventListener('click', function() {
-    var type, _i, _len, _ref;
-    navTrigger.classList.toggle('menu-is-open');
-    _ref = ['webkitTransitionEnd', 'otransitionend', 'oTransitionEnd', 'msTransitionEnd', 'transitionend'];
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      type = _ref[_i];
-      popupNavList.removeEventListener(type);
-    }
-    return popupNavList.classList.toggle('is-visible');
+  var navList, popupNav, trigger;
+  popupNav = document.querySelector('.popup-nav');
+  trigger = popupNav.querySelector('.trigger');
+  navList = popupNav.querySelector('nav ul');
+  return trigger.addEventListener('click', function() {
+    trigger.classList.toggle('menu-is-open');
+    return navList.classList.toggle('is-visible');
   });
 })();
 
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -66,57 +66,14 @@
   fadeIn()
 )()
 
-(-> # Fixed menu
-  offset = 900
+(-> # Popup menu
+  popupNav = document.querySelector '.popup-nav'
+  trigger = popupNav.querySelector '.trigger'
+  navList = popupNav.querySelector 'nav ul'
 
-  popupNav = document.querySelector '.popup-nav.fixed'
-  navTrigger = popupNav.querySelector '.trigger'
-  popupNavList = popupNav.querySelector 'nav ul'
-
-  checkMenu = ->
-    if window.scrollY > offset and not popupNav.classList.contains 'visible'
-      popupNav.classList.add 'visible'
-
-      animCallback = (type) ->
-        popupNavList.classList.add 'has-transitions'
-        navTrigger.removeEventListener type, animCallback
-
-      for type in ['webkitAnimationEnd', 'oanimationend', 'msAnimationEnd', 'animationend']
-        navTrigger.addEventListener type, animCallback.bind(null, type)
-
-    else if window.scrollY <= offset - 500
-
-      if popupNavList.classList.contains 'is-visible'
-        popupNavList.classList.add 'is-hidden'
-
-        cb = (type) ->
-          popupNavList.classList.remove c for c in ['is-visible', 'is-hidden', 'has-transitions']
-          popupNav.classList.remove 'visible'
-          navTrigger.classList.remove 'menu-is-open'
-          navTrigger.removeEventListener type, cb
-
-        for type in ['webkitTransitionEnd', 'otransitionend', 'oTransitionEnd', 'msTransitionEnd', 'transitionend']
-          popupNavList.addEventListener type, cb.bind(null, type)
-
-      else if popupNavList.classList.contains 'is-visible'
-        popupNavList.classList.remove c for c in ['is-visible', 'has-transitions']
-        popupNav.classList.remove 'visible'
-        navTrigger.classList.remove 'menu-is-open'
-
-      else
-        popupNav.classList.remove 'visible'
-        popupNavList.classList.remove 'has-transitions'
-
-  checkMenu()
-  window.onscroll = checkMenu
-
-  navTrigger.addEventListener 'click', ->
-    navTrigger.classList.toggle 'menu-is-open'
-
-    for type in ['webkitTransitionEnd', 'otransitionend', 'oTransitionEnd', 'msTransitionEnd', 'transitionend']
-      popupNavList.removeEventListener type
-
-    popupNavList.classList.toggle 'is-visible'
+  trigger.addEventListener 'click', ->
+    trigger.classList.toggle 'menu-is-open'
+    navList.classList.toggle 'is-visible'
 )()
 
 (-> # Choose a header background
@@ -124,7 +81,7 @@
   document.querySelector('.fullscreen').style.backgroundImage = "url('../img/background-#{randomInt}.jpg')"
 )()
 
-(-> # Resize header
+(-> # Make header size static
   fullscreen = document.querySelector '.fullscreen'
   belowFullscreen = document.querySelector '.below-fullscreen'
   fullscreen.style.height = fullscreen.offsetHeight
