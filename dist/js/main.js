876c7882f67885e1fd49cf0a5f42db3a102d58a2
Alexandru Rosianu <rosian_alex@yahoo.com>
2014-12-08 20:39:32 +0200

    Finished smooth scrolling

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,22 @@
 (function() {
-  var activeItemSelected, activeMenuItem, item, menuItems, mouseOut, mouseOver, timeout, update, _i, _len;
-  menuItems = document.querySelectorAll('nav ul li');
-  activeMenuItem = 0;
-  activeItemSelected = true;
+  var belowFullscreen, fadeIn, fadeOut, fullscreen, height, i, item, j, lastIndex, menuItems, mouseOut, mouseOver, navList, nextIndex, phrases, popupNav, randomInt, selectedMenuItem, selectedMenuItemHighlighted, size, timeout, titleElement, trigger, update, x, _i, _len;
+  menuItems = document.querySelectorAll('header > nav ul li');
+  selectedMenuItem = 0;
+  selectedMenuItemHighlighted = true;
   update = function() {
-    if (activeItemSelected) {
-      return menuItems[activeMenuItem].classList.add('active');
+    if (selectedMenuItemHighlighted) {
+      return menuItems[selectedMenuItem].classList.add('active');
     } else {
-      return menuItems[activeMenuItem].classList.remove('active');
+      return menuItems[selectedMenuItem].classList.remove('active');
     }
   };
   mouseOver = function(event) {
-    activeItemSelected = event.target.parentElement === menuItems[activeMenuItem];
+    selectedMenuItemHighlighted = event.target.parentElement === menuItems[selectedMenuItem];
     return update();
   };
   mouseOut = function() {
     var timeout;
-    activeItemSelected = true;
+    selectedMenuItemHighlighted = true;
     clearTimeout(timeout);
     return timeout = setTimeout(update, 300);
   };
@@ -26,11 +26,7 @@
     item.addEventListener('mouseout', mouseOut, false);
   }
   timeout = null;
-  return update();
-})();
-
-(function() {
-  var fadeIn, fadeOut, i, j, lastIndex, nextIndex, phrases, titleElement, x;
+  update();
   phrases = ['FULL-STACK WEB CODER', 'ANDROID DEVELOPER', 'iOS BEGINNER', 'MAC USER', 'TECHNOLOGY ENTHUSIAST', 'OCCASIONAL HACKER', 'COMMAND-LINE WIZARD'];
   titleElement = document.querySelector('.hero h2:last-child');
   lastIndex = -1;
@@ -57,92 +53,49 @@
     titleElement.style.opacity = 0;
     return setTimeout(fadeIn, 250);
   };
-  return fadeIn();
-})();
-
-(function() {
-  var navList, popupNav, trigger;
+  fadeIn();
   popupNav = document.querySelector('.popup-nav');
   trigger = popupNav.querySelector('.trigger');
   navList = popupNav.querySelector('nav ul');
-  return trigger.addEventListener('click', function() {
+  trigger.addEventListener('click', function() {
     trigger.classList.toggle('menu-is-open');
     return navList.classList.toggle('is-visible');
   });
-})();
-
-(function() {
-  var randomInt, size;
   randomInt = Math.floor(Math.random() * 3);
   size = 1920;
-  if (window.innerWidth <= 1440 && window.innerHeight <= 1024) {
+  if (innerWidth <= 1440 && innerHeight <= 1024) {
     size = 1440;
   }
-  if (window.innerWidth <= 768 && window.innerHeight <= 768) {
+  if (innerWidth <= 768 && innerHeight <= 768) {
     size = 768;
   }
-  if (window.innerWidth <= 480 && window.innerHeight <= 480) {
+  if (innerWidth <= 480 && innerHeight <= 480) {
     size = 480;
   }
-  return document.querySelector('.fullscreen').style.backgroundImage = "url('../img/background-" + randomInt + "@" + size + ".jpg')";
-})();
-
-(function() {
-  return window.scrollTo = function(to) {
-    var animateScroll, change, currentTime, doc, duration, easeInOutQuad, firefox, increment, msie, requestAnimFrame, start;
-    firefox = navigator.userAgent.indexOf('Firefox');
-    msie = navigator.userAgent.indexOf('MSIE');
-    doc = firefox !== -1 || msie !== -1 ? document.documentElement : document.body;
-    easeInOutQuad = function(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) {
-        return c / 2 * t * t + b;
-      } else {
-        return -c / 2 * ((t - 1) * (t - 3) - 1) + b;
-      }
-    };
-    requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function(callback) {
-      return window.setTimeout(callback, 1000 / 60);
-    };
-    start = doc.scrollTop;
-    change = to.offsetTop - start;
-    console.log(to, to.offsetTop, start, change);
-    currentTime = 0;
-    increment = 20;
-    duration = 800;
-    animateScroll = function() {
-      var val;
-      currentTime += increment;
-      val = easeInOutQuad(currentTime, start, change, duration);
-      doc.scrollTop = val;
-      if (currentTime < duration) {
-        return requestAnimFrame(animateScroll);
-      }
-    };
-    return animateScroll();
-  };
-})();
-
-(function() {
-  var belowFullscreen, fullscreen, height, item, onClick, _i, _len, _ref, _results;
+  document.querySelector('.fullscreen').style.backgroundImage = "url('../img/background-" + randomInt + "@" + size + ".jpg')";
   fullscreen = document.querySelector('.fullscreen');
   belowFullscreen = document.querySelector('.below-fullscreen');
   height = fullscreen.offsetHeight;
-  if (window.innerWidth >= 480 && window.innerHeight < 600) {
+  if (innerWidth >= 480 && innerHeight < 600) {
     height = 600;
-  } else if (window.innerHeight < 400) {
+  } else if (innerHeight < 400) {
     height = 400;
   }
   fullscreen.style.height = height + 'px';
   belowFullscreen.style.top = height + 'px';
-  onClick = function() {
-    return window.scrollTo(document.querySelector("a[name='" + (this.hash.substring(1)) + "']"));
-  };
-  _ref = document.querySelectorAll('nav a');
-  _results = [];
-  for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-    item = _ref[_i];
-    _results.push(item.onclick = onClick);
-  }
-  return _results;
+  return smoothScroll.init({
+    callbackAfter: function(toggle) {
+      var _j, _len1;
+      for (i = _j = 0, _len1 = menuItems.length; _j < _len1; i = ++_j) {
+        item = menuItems[i];
+        if (item.childNodes[0] === toggle) {
+          menuItems[selectedMenuItem].classList.remove('active');
+          selectedMenuItem = i;
+          menuItems[selectedMenuItem].classList.add('active');
+        }
+      }
+      trigger.classList.remove('menu-is-open');
+      return navList.classList.remove('is-visible');
+    }
+  });
 })();
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -1,21 +1,22 @@
-(-> # Menu
-  menuItems = document.querySelectorAll 'nav ul li'
+(->
+  # Menu
+  menuItems = document.querySelectorAll 'header > nav ul li'
 
-  activeMenuItem = 0
-  activeItemSelected = true
+  selectedMenuItem = 0
+  selectedMenuItemHighlighted = true
 
   update = ->
-    if activeItemSelected
-      menuItems[activeMenuItem].classList.add 'active'
+    if selectedMenuItemHighlighted
+      menuItems[selectedMenuItem].classList.add 'active'
     else
-      menuItems[activeMenuItem].classList.remove 'active'
+      menuItems[selectedMenuItem].classList.remove 'active'
 
   mouseOver = (event) ->
-    activeItemSelected = event.target.parentElement == menuItems[activeMenuItem]
+    selectedMenuItemHighlighted = event.target.parentElement == menuItems[selectedMenuItem]
     update()
 
   mouseOut = ->
-    activeItemSelected = true
+    selectedMenuItemHighlighted = true
     clearTimeout timeout
     timeout = setTimeout update, 300
 
@@ -25,9 +26,8 @@
 
   timeout = null
   update()
-)()
 
-(-> # Title animation
+  # Title animation
   phrases = [
     'FULL-STACK WEB CODER',
     'ANDROID DEVELOPER',
@@ -64,9 +64,8 @@
     setTimeout fadeIn, 250
 
   fadeIn()
-)()
 
-(-> # Popup menu
+  # Popup menu
   popupNav = document.querySelector '.popup-nav'
   trigger = popupNav.querySelector '.trigger'
   navList = popupNav.querySelector 'nav ul'
@@ -74,65 +73,39 @@
   trigger.addEventListener 'click', ->
     trigger.classList.toggle 'menu-is-open'
     navList.classList.toggle 'is-visible'
-)()
 
-(-> # Choose a header background
+  # Choose a header background
   randomInt = Math.floor(Math.random() * 3)
   size = 1920
 
-  size = 1440 if window.innerWidth <= 1440 and window.innerHeight <= 1024
-  size = 768  if window.innerWidth <= 768  and window.innerHeight <= 768
-  size = 480  if window.innerWidth <= 480  and window.innerHeight <= 480
+  size = 1440 if innerWidth <= 1440 and innerHeight <= 1024
+  size = 768  if innerWidth <= 768  and innerHeight <= 768
+  size = 480  if innerWidth <= 480  and innerHeight <= 480
 
   document.querySelector('.fullscreen').style.backgroundImage = "url('../img/background-#{randomInt}@#{size}.jpg')"
-)()
 
-(-> # Scrolling helper
-  window.scrollTo = (to) ->
-    firefox = navigator.userAgent.indexOf('Firefox')
-    msie = navigator.userAgent.indexOf('MSIE')
-    doc = if firefox != -1 or msie != -1 then document.documentElement else document.body
-
-    easeInOutQuad = (t, b, c, d) ->
-      t /= d/2
-      if t < 1
-        c/2*t*t + b
-      else
-        -c/2 * ((t-1)*(t-3) - 1) + b
-
-    requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || (callback) -> window.setTimeout(callback, 1000 / 60)
-
-    start = doc.scrollTop
-    change = to.offsetTop - start
-    console.log to, to.offsetTop, start, change
-    currentTime = 0
-    increment = 20
-    duration = 800
-
-    animateScroll = ->
-      currentTime += increment
-      val = easeInOutQuad(currentTime, start, change, duration)
-      doc.scrollTop = val
-
-      if currentTime < duration
-        requestAnimFrame(animateScroll)
-
-    animateScroll()
-)()
-
-(-> # Make header size static
+  # Make header size static
   fullscreen = document.querySelector '.fullscreen'
   belowFullscreen = document.querySelector '.below-fullscreen'
   height = fullscreen.offsetHeight
 
-  if window.innerWidth >= 480 && window.innerHeight < 600
+  if innerWidth >= 480 && innerHeight < 600
     height = 600
-  else if window.innerHeight < 400
+  else if innerHeight < 400
     height = 400
 
   fullscreen.style.height = height + 'px'
   belowFullscreen.style.top = height + 'px'
 
-  onClick = -> window.scrollTo document.querySelector("a[name='#{this.hash.substring(1)}']")
-  item.onclick = onClick for item in document.querySelectorAll 'nav a'
+  # Anchor smooth scrolling
+  smoothScroll.init
+    callbackAfter: (toggle) ->
+      for item, i in menuItems
+        if item.childNodes[0] == toggle
+          menuItems[selectedMenuItem].classList.remove 'active'
+          selectedMenuItem = i
+          menuItems[selectedMenuItem].classList.add 'active'
+
+      trigger.classList.remove 'menu-is-open'
+      navList.classList.remove 'is-visible'
 )()
