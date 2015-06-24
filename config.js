3ba118af7e64bf53d0bda3f5860c05e5500a8b23
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-06-24 16:51:55 +0300

    Changes url

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ if (process.env.OPENSHIFT_MYSQL_DB_HOST != undefined) {
         // When running Ghost in the wild, use the production environment
         // Configure your URL and mail settings here
         production: {
-            url: 'http://'+process.env.OPENSHIFT_APP_DNS,
+            url: 'http://blog.aluxian.com',
             mail: {},
             database: {
                 client: 'mysql',
