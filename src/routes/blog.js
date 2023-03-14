2e67e7e4bbe38f6dcbd994dec20b3078274ecc67
Alexandru Rosianu <me@aluxian.com>
2023-03-14 23:38:31 +0000

    all tests passed, blog implemented

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -5,9 +5,28 @@ export const match = (url) => url.pathname.startsWith("/blog/");
 export async function fetch(request, env) {
   const url = new URL(request.url);
 
+  /**
+   * @type {{id: string, title: string, text: string, tags: string[], link: string, createdAt: string, modifiedAt: string, html: string, files: string[]}[]}
+   */
+  const posts = (await env.DB.get(`blog:posts`, "json")) || [];
+
   // list all posts
   if (url.pathname === "/blog/") {
-    return new Response("list", {
+    const html = `
+      <h1>Blog</h1>
+      <ul>
+        ${posts
+          .map(
+            (post) => `
+          <li>
+            <a href="/blog/${post.slug}">${post.title}</a>
+          </li>
+        `
+          )
+          .join("")}
+      </ul>
+    `;
+    return new Response(html, {
       headers: {
         "Content-Type": "text/html",
       },
@@ -16,11 +35,10 @@ export async function fetch(request, env) {
 
   // show a post
   const slug = url.pathname.replace("/blog/", "");
-  const posts = await env.DB.get(`blog:posts`, "json");
-  const post = posts.find((post) => post.slug === slug);
+  const post = posts.find((post) => post.id === slug);
 
   if (post) {
-    return new Response(JSON.stringify(post), {
+    return new Response(post.html, {
       headers: {
         "Content-Type": "text/html",
       },
--- src/routes/blog.test.js
+++ src/routes/blog.test.js
@@ -1,4 +1,8 @@
 import test from "ava";
 import * as worker from "../testing/wrangler/worker.js";
 
-test.skip("/blog/ should return list of posts", async (t) => {});
+test.serial("/blog/ should return list of posts", async (t) => {
+  const res = await worker.fetch("/blog/");
+  const html = await res.text();
+  t.true(html.includes("<h1>Blog</h1>"), "should contain <h1> title");
+});
